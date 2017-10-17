8a4a633b6d4339fe92c8ecdc7e47feeb131e8c22
Karn <karnseaman@gmail.com>
2017-10-17 01:44:43 -0700

    Worked on creating a window. Can now create a window with a button.

---
+++ Examples/stl-example/opener/Comparisoft/Comparisoft/Main.cpp
@@ -0,0 +1,230 @@
+// GT_HelloWorldWin32.cpp  
+// compile with: /D_UNICODE /DUNICODE /DWIN32 /D_WINDOWS /c  
+
+#include <windows.h>  
+#include <stdlib.h>  
+#include <string.h>  
+#include <tchar.h> 
+#include <shobjidl.h>
+
+// Global variables  
+
+// The main window class name.  
+static TCHAR szWindowClass[] = _T("win32app");
+
+// The string that appears in the application's title bar.  
+static TCHAR szTitle[] = _T("Comparisoft");
+
+HINSTANCE hInst;
+
+// Forward declarations of functions included in this code module:  
+LRESULT CALLBACK WndProc(HWND, UINT, WPARAM, LPARAM);
+
+int CALLBACK WinMain(
+	_In_ HINSTANCE hInstance,
+	_In_ HINSTANCE hPrevInstance,
+	_In_ LPSTR     lpCmdLine,
+	_In_ int       nCmdShow
+)
+{
+	WNDCLASSEX wcex;
+
+	wcex.cbSize = sizeof(WNDCLASSEX);
+	wcex.style = CS_HREDRAW | CS_VREDRAW;
+	wcex.lpfnWndProc = WndProc;
+	wcex.cbClsExtra = 0;
+	wcex.cbWndExtra = 0;
+	wcex.hInstance = hInstance;
+	wcex.hIcon = LoadIcon(hInstance, MAKEINTRESOURCE(IDI_APPLICATION));
+	wcex.hCursor = LoadCursor(NULL, IDC_ARROW);
+	wcex.hbrBackground = (HBRUSH)(COLOR_WINDOW + 1);
+	wcex.lpszMenuName = NULL;
+	wcex.lpszClassName = szWindowClass;
+	wcex.hIconSm = LoadIcon(wcex.hInstance, MAKEINTRESOURCE(IDI_APPLICATION));
+
+	if (!RegisterClassEx(&wcex))
+	{
+		MessageBox(NULL,
+			_T("Call to RegisterClassEx failed!"),
+			_T("Win32 Guided Tour"),
+			NULL);
+
+		return 1;
+	}
+
+	hInst = hInstance; // Store instance handle in our global variable  
+
+					   // The parameters to CreateWindow explained:  
+					   // szWindowClass: the name of the application  
+					   // szTitle: the text that appears in the title bar  
+					   // WS_OVERLAPPEDWINDOW: the type of window to create  
+					   // CW_USEDEFAULT, CW_USEDEFAULT: initial position (x, y)  
+					   // 500, 100: initial size (width, length)  
+					   // NULL: the parent of this window  
+					   // NULL: this application does not have a menu bar  
+					   // hInstance: the first parameter from WinMain  
+					   // NULL: not used in this application  
+	HWND hWnd = CreateWindow(
+		szWindowClass,
+		szTitle,
+		WS_OVERLAPPEDWINDOW,
+		CW_USEDEFAULT, CW_USEDEFAULT,
+		500, 100,
+		NULL,
+		NULL,
+		hInstance,
+		NULL
+	);
+
+	if (!hWnd)
+	{
+		MessageBox(NULL,
+			_T("Call to CreateWindow failed!"),
+			_T("Win32 Guided Tour"),
+			NULL);
+
+		return 1;
+	}
+
+	// The parameters to ShowWindow explained:  
+	// hWnd: the value returned from CreateWindow  
+	// nCmdShow: the fourth parameter from WinMain  
+	ShowWindow(hWnd,
+		nCmdShow);
+	UpdateWindow(hWnd);
+
+	// Main message loop:  
+	MSG msg;
+	while (GetMessage(&msg, NULL, 0, 0))
+	{
+		TranslateMessage(&msg);
+		DispatchMessage(&msg);
+	}
+
+	return (int)msg.wParam;
+}
+
+//  
+//  FUNCTION: WndProc(HWND, UINT, WPARAM, LPARAM)  
+//  
+//  PURPOSE:  Processes messages for the main window.  
+//  
+//  WM_PAINT    - Paint the main window  
+//  WM_DESTROY  - post a quit message and return  
+//  
+//  
+LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
+{
+	PAINTSTRUCT ps;
+	HDC hdc;
+	TCHAR greeting[] = _T("Hello, World!");
+
+	switch (message)
+	{
+	case WM_CREATE:
+
+		//create a button
+		CreateWindow(
+			
+			//thing to create
+			TEXT("button"),
+
+			//text in the button
+			TEXT("Select file 1"),
+
+			//Styles
+			WS_VISIBLE|WS_CHILD,
+
+			//x, y, width, height
+			20, 20, 80, 25,
+
+			//parent window
+			hWnd,
+
+			//unique identifier
+			(HMENU) 1,
+
+			NULL, NULL
+		);
+
+		break;
+
+	case WM_COMMAND:
+
+		if (LOWORD(wParam) == 1) {
+			MessageBox(hWnd, L"Woot! *.*/", L"Ooooooh", MB_OK | MB_ICONINFORMATION);
+		}
+
+		break;
+
+	case WM_PAINT:
+		hdc = BeginPaint(hWnd, &ps);
+
+		// Here your application is laid out.  
+		// For this introduction, we just print out "Hello, World!"  
+		// in the top left corner.  
+		TextOut(hdc,
+			5, 5,
+			greeting, _tcslen(greeting));
+		// End application-specific layout section.  
+
+		EndPaint(hWnd, &ps);
+		break;
+	case WM_DESTROY:
+		PostQuitMessage(0);
+		break;
+	default:
+		return DefWindowProc(hWnd, message, wParam, lParam);
+		break;
+	}
+
+	return 0;
+}
+
+
+/*#include <windows.h>
+#include <shobjidl.h> 
+
+int WINAPI wWinMain(HINSTANCE hInstance, HINSTANCE, PWSTR pCmdLine, int nCmdShow)
+{
+	HRESULT hr = CoInitializeEx(NULL, COINIT_APARTMENTTHREADED |
+		COINIT_DISABLE_OLE1DDE);
+	if (SUCCEEDED(hr))
+	{
+		IFileOpenDialog *pFileOpen;
+
+		// Create the FileOpenDialog object.
+		hr = CoCreateInstance(CLSID_FileOpenDialog, NULL, CLSCTX_ALL,
+			IID_IFileOpenDialog, reinterpret_cast<void**>(&pFileOpen));
+
+		if (SUCCEEDED(hr))
+		{
+			// Show the Open dialog box.
+			hr = pFileOpen->Show(NULL);
+
+			// Get the file name from the dialog box.
+			if (SUCCEEDED(hr))
+			{
+				IShellItem *pItem;
+				hr = pFileOpen->GetResult(&pItem);
+				if (SUCCEEDED(hr))
+				{
+					PWSTR pszFilePath;
+					hr = pItem->GetDisplayName(SIGDN_FILESYSPATH, &pszFilePath);
+
+					// Display the file name to the user.
+					if (SUCCEEDED(hr))
+					{
+						MessageBox(NULL, pszFilePath, L"File Path", MB_OK);
+						CoTaskMemFree(pszFilePath);
+					}
+					pItem->Release();
+				}
+			}
+			pFileOpen->Release();
+		}
+		CoUninitialize();
+	}
+	return 0;
+}
+*/
