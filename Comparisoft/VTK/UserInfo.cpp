8cc81db6fcbd68b79f52f9358e61281be408db5f
danaack <danak@shaw.ca>
2018-01-08 18:36:38 -0800

    Begin background functions to store client information from parsing

---
+++ Comparisoft/VTK/UserInfo.cpp
@@ -0,0 +1,12 @@
+/******************************************************************************/
+/**
+@file		UserInfo.cpp
+@reference	UserInfo.h
+@author 	Dana Klamut
+
+@details	This file handles the parsing of the client/patient information
+ 			from the uploaded reference and production file paths.
+*/
+/******************************************************************************/
+
+#include "UserInfo.h"
